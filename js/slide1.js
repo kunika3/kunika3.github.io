4ad2bee3b399fd8d3bffadac2905682afaa16455
Sood <c-ksood@ksood-MacBookPro.local>
2021-07-30 14:59:57 -0700

    Final

--- js/slide1.js
+++ js/slide1.js
@@ -1,7 +1,4 @@
-var integratedData= [];
-
 (function () {
-    var date = 'June-2021';
     var margin = { top: 50, left: 50, right: 50, bottom: 50},
     height = 600 - margin.top - margin.bottom,
     width = 800 - margin.left - margin.right;
@@ -24,7 +21,7 @@ var integratedData= [];
         var vaccine1data = values[2];
         var populationdata = values[3];
 
-        console.log(populationdata);
+        sessionStorage.setItem( 'vaccineByDateToPass', JSON.stringify(vaccine1data) );
         var integratedDatavaccine = [];
         var dateData = [];
         var x=0;
@@ -47,13 +44,12 @@ var integratedData= [];
             var formatDate = d3.timeFormat("%B-%Y");
             element.date = formatDate(parseDate(element.date));
         });
-
+        var date = covid1data[covid1data.length-1].date;
         vaccine1data.forEach(function(element, key) {
             var parseDate = d3.timeParse("%Y-%m-%d");
             var formatDate = d3.timeFormat("%B-%Y");
             element.Date = formatDate(parseDate(element.Date));
         });
-
         localStorage.setItem( 'vaccineToPass', JSON.stringify(vaccine1data) );
         var grouped_data = d3.group(covid1data,
             d => d.state,
@@ -70,88 +66,6 @@ var integratedData= [];
             d => d.Date );
 
 
-
-        // Format vaccine data
-        grouped_vaccine_data.forEach(function(element, key, value) {
-            var tempData = [];
-
-            element.forEach(function(data, index) {
-
-                    sortData[index] = {'fully_vaccinated' : data[data.length-1].People_Fully_Vaccinated, 'partially_vaccinated' : data[data.length-1].People_Partially_Vaccinated, 'Date_vaccine': data[data.length-1].Date,
-                    'lat': data[data.length-1].Lat, 'long': data[data.length-1].Long_};
-
-                    tempData.push(sortData[index]);
-            });
-            integratedDatavaccine[key] = tempData;
-
-        });
-
-        //Format covid data
-        grouped_data.forEach(function(element, key, value) {
-            var tempData = [];
-
-            element.forEach(function(data, index) {
-                if(!dateData.includes(index)) {
-                    dateData.push(index);
-                }
-
-                sortData[index] = {'cases' : data[data.length-1].cases, 'state': data[data.length-1].state, 'date': data[data.length-1].date};
-
-                tempData.push(sortData[index]);
-            });
-            integratedData[key] = tempData;
-        });
-
-        //create one single data
-        for (var id in integratedData){
-            for (var idv in integratedDatavaccine) {
-                if (id === idv) {
-                    for (val in integratedData[id]) {
-
-                        var comDate = integratedDatavaccine[idv].filter(word => {
-                            return word.Date_vaccine === integratedData[id][val].date} );
-                        integratedData[id][val].lat = integratedDatavaccine[idv][0].lat;
-                        integratedData[id][val].long = integratedDatavaccine[idv][0].long;
-                        if(comDate && comDate[0] && comDate[0] !== undefined) {
-                            integratedData[id][val].fully_vaccinated = comDate[0].fully_vaccinated;
-                            integratedData[id][val].partially_vaccinated = comDate[0].partially_vaccinated;
-                        }
-
-                    }
-
-                }
-            }
-
-        }
-
-        //integrate with state data
-        // states.forEach(function(element, key) {
-        //     element['properties'].covid = integratedData[element.properties.name];
-        // });
-
-        // var list = d3.select('body').append("select");
-
-        // list.selectAll("option")
-        // .data(d3.map(grouped_data_byDate, function(d){return d;}))
-        // .enter()
-        // .append("option")
-        // .attr("value", function(d) { console.log(d, d[0]); return d[0];})
-        // .text(function(d){console.log(d, d[0]);return d[0];})
-        // .attr("value",function(d){return d;})
-        // .on('change',function(){
-        //     onchange();
-        // });
-
-
-        // d3.select("body").selectAll("option")
-        // .data(d3.map(grouped_data_byDate, function(d){return d;}))
-        // .enter()
-        // .append("option")
-        // .text(function(d){console.log(d, d[0]);return d[0];})
-        // .attr("value",function(d){return d;});
-
-
-
         dropdown.selectedIndex = dateData.length;
 
         let max = d3.max(covid1data, function (d, i) {
@@ -162,18 +76,9 @@ var integratedData= [];
             return d.cases;
         });
 
-
-
-        // var colorScale = d3.scaleLinear()
-        //     .domain(0, 10000, 500000, 800000, 1000000)
-        //     .range(["blue", "aqua", "purple", "indigo", "green"]);
-    //     var colorScale = d3.scaleThreshold()
-    // .domain([min,max])
-    // .range(d3.schemeBlues[30]);
-
-    var colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(['white','pink', 'red']);
+        var colorScale = d3.scaleThreshold()
+            .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+            .range(['white','pink', 'red']);
 
 
 
@@ -187,7 +92,7 @@ var integratedData= [];
                 });
 
 
-            var options = select
+        var options = select
             .selectAll('option')
                 .data(d3.map(grouped_data_byDate, function(d, index){ return d;})).enter()
                 .append('option')
@@ -195,173 +100,34 @@ var integratedData= [];
                 .attr("index",function(d, index){return index})
                 .attr("value",function(d){return d;});
 
+        options.property('selected', function(d){
+            return d[0] === date;
+        });
+
         function onchange(value, text, index) {
             d3.selectAll("svg").remove();
             console.log(value, text, index);
-            selectValue = d3.select('select').property('value')
-            selectIndex = d3.select('option').property('index')
-            console.log(selectIndex);
+            date = text;
+            options.property('selected', function(d){
+                return d[0] === date;
+            });
 
             getCombinedDataForMap(covid1data, vaccine1data, populationdata, states, date);
             getSvgPath(states, path, colorScale, populationdata)
 
-            // g.selectAll( "path" )
-            //     .data( integratedData)
-            //     .enter()
-            //     .append( "path" )
-            //     .attr('d', path)
-            //     .attr('fill', function(d) {
-            //         console.log(d);
-            //     })
-
-
-
-            //     .attr( "stroke", "none" )
-            //     .attr( "opacity", 0.5)
-            //     .attr( "d", path )
-            //     .attr("class", "incident")
         };
 
         getCombinedDataForMap(covid1data, vaccine1data, populationdata, states, date);
         getSvgPath(states, path, colorScale, populationdata)
-        // var arry = d3.map(grouped_data_byDate, function(d, index){ return d;});
 
-        // d3.json("https://gist.githubusercontent.com/Bradleykingz/3aa5206b6819a3c38b5d73cb814ed470/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json", function (error, uState) {
-        //     if (error) throw error;
-        //     _(uState.features)
-        //         .keyBy('properties.name')
-        //         .merge(_.keyBy(covid1data, 'State'))
-        //         .values()
-        //         .value();
-
-        // console.log(uState);
-        // svg.selectAll('path')
-        //     .data(states.features)
-        //     .enter()
-        //     .append('path')
-        //     .attr("d", path)
-        //     .attr('class', 'state')
-        //     .attr("value",function(d){return d.state;})
-        //     .attr("date",function(d){
-        //         return d.date;})
-        //     .attr("cases",function(d){return d.cases;})
-        //     .style("pointer-events","all")
-        //     .style("fill", function(d) {
-
-        //         //The join works
-        //         console.log(colorScale(d.cases), d.cases)
-        //         if (d.cases) {
-        //             return colorScale(d.cases);
-        //         } else {
-        //             return "#ccc";
-        //         }
-        //     })
-        //     .on("mouseover", function(event, d) {
-        //         console.log('hello');
-        //         percent = Number(d.People_Fully_Vaccinated)/Number(d.population.replace(/,/g,''))*100;
-        //         console.log('hi', percent);
-        //         tooltip.style("display", "flex")
-        //                .html(function() {
-        //           tooltip.attr("country", d.state);
-        //           return `${d.state} Cases: ${d.cases},
-        //                     Deaths: ${d.deaths}, FullyVaccinated%: ${percent || 0}`;})
-        //                .style("left", (event.pageX + 10)+"px")
-        //                .style("top", (event.pageY - 28) + "px")
-        //       })
-        //       .on("mouseout", () => {
-        //         tooltip.style("display", "none")
-        //     });
-            // .on('mouseover', (event, d) => {
-            //     console.log(d.cases);
-            //     var xPosition = d3.select(this).attr("px");
-            //     // var yPosition = parseFloat(d3.select(this).attr("cy"));
-
-            //     d3.select('body')
-            //     .append("div")
-            //     .attr("id", "tooltip")
-            //     .transition().duration(200)
-            //     .style('opacity', 0.9)
-            //     .style('left', (d3.xPosition) + 'px')
-            //     // .style('top', (d3.event.pageY / 1.5) + 'px')
-
-            //     // // .attr("y", yPosition)
-            //     .text( "Confirmed cases "+d.cases);
-
-            // })
-                //   tooltipCases
-                //     .attr('data-year', d.cases)
-                //     .text("The exact value of<br>this cell is: " + d.cases)
-                //     .style("left", (d3.pointer(this)[0]+70) + "px")
-                    // .style("top", (d3.pointer(this)[1]) + "px")
-            // })
-            // .on("mouseleave", function(d){
-            //     tooltipCases.style("opacity", 0)
-            // })
-
-
-
-
-        // svg.append('g')
-        //     .attr('class', 'state')
-        //     .selectAll('path')
-        //     .data(states.features)
-        //     .enter()
-        //     .append('path')
-        //     .style("fill", function(d) {
-
-        //         //The join works
-        //         console.log(d, d.cases)
-
-        //              if (d.cases) {
-
-        //                  return colorScale(d.cases);
-        //              } else {
-        //                  return "black";
-        //              }
-        //     })
-
-        // svg.selectAll( "path" )
-        //     .selectAll('.state')
-        //     .data(states)
-        //     .enter()
-        //     .append('path')
-        //     .attr('class', 'state')
-        //     .attr('d', path)
-        //     .style("fill", "none")
-        //     .style("stroke", "black")
-        //     .style("stroke-width", "2");
-
-
-
-
-                // d.total =  d.covid[d.covid.length-1].cases || 0;
-                // return colorScale(d.total);
-            // .append("circle", ".pin")
-            // .attr("r", 2)
-            // .raise()
-            // .attr("transform", function(d) {
-            //     console.log(d);
-            //     return "translate(" + projection([
-            //     +d.Longitude,
-            //     +d.Latitude
-            //     ]) + ")";
-            // });
 
 
     });
-    let dropdown = $('#locality-dropdown');
+    let dropdown = $('#state-dropdown');
 
     dropdown.empty();
 
     dropdown.append('<option disabled>Choose Month-Year</option>');
-
-    $.getScript('js/story1.js', function()
-    {
-        // script is now loaded and executed.
-        // put your dependent JS here.
-        console.log('Loaded');
-    });
-
     function getCombinedDataForMap (covid1data, vaccine1data, populationdata, states, date) {
 
         console.log(vaccine1data);
@@ -374,10 +140,6 @@ var integratedData= [];
         });
 
         console.log(sorted_countries_byDate, sorted_countries_byvaccineDate);
-
-        // var more_sort = sorted_countries_byDate.filter(function(d){
-        //     console.log(d3.map(d[1], function(d, index){ return d;}));
-        // })
 
        _(states.features)
             .keyBy('properties.name')
@@ -400,16 +162,11 @@ var integratedData= [];
         .values()
         .value();
 
-        console.log(states);
-        // _(states.features)
-        //     .keyBy('properties.name')
-        //     .merge(_.keyBy(sorted_countries_byDate, 'state'))
-        //     .values()
-        //     .value();
-
 
     }
+
     function getSvgPath(states, path, colorScale, populationdata) {
+
         var  svg = d3.select('#canvas')
             .append('svg')
             .attr('height', height + margin.top + margin.bottom)
@@ -452,7 +209,26 @@ var integratedData= [];
               .on("mouseout", () => {
                 tooltip.style("display", "none")
             });
+
+            svg.selectAll('g.legend')
+            .data(colorScale.range().reverse())
+            .enter()
+            .append('g').attr('class', 'legend')
+            .append('rect')
+            .attr("x", width - 780)
+            .attr("y", function(d, i) {
+                return i * 20;
+            }).attr("width", 10)
+            .attr("height", 10)
+            .style("fill", function(d){return d;})
+            .append('text')
+            .attr("x", width - 765) //leave 5 pixel space after the <rect>
+            .attr("y", function(d, i) {
+                return i * 20;
+            }).text(function(d){
+                return (d);
+            })
+
     }
 
 })();
-
--- js/slide2.js
+++ js/slide2.js
@@ -1,6 +1,5 @@
 (function() {
-    let state = 'Washington';
-    let date = 'July-2021';
+    let state = 'California';
     var margin = { top: 50, left: 50, right: 50, bottom: 50},
     height = 600 - margin.top - margin.bottom,
     width = 800- margin.left - margin.right;
@@ -13,9 +12,6 @@
     dropdown.append('<option disabled>Choose Month-Year</option>');
     var covid =[];
     var stateData={}
-    // for (user in vaccineusers) {
-    //     covid.push(vaccineusers[user].properties.covid);
-    // }
 
     var x = d3.scaleTime()
       .domain(d3.extent(vaccinedata, function(d) { return new Date(d.Date); }))
@@ -29,69 +25,56 @@
         d => d.Province_State,
         d => d.Date );
 
+
     var dropdownData = d3.map(grouped_vaccine_data, function(d, index){ return d;});
 
     console.log(dropdownData);
 
     var sorted_countries_byvaccineState = getFilteredData(states, vaccinedata, state);
 
-    // let result = covid.map(a => stateData[a[0]['state']] = a[0]['state']);
-
     dropdown.selectedIndex = sorted_countries_byvaccineState.length;
 
-        var select = d3.select('body')
-            .append('select')
-                .attr('class','select')
-                .on('change',function()
-                { onchange(this.value, this.options[this.selectedIndex].text, this.selectedIndex)
+    var select = d3.select('body')
+        .append('select')
+        .attr('class','select')
+        .on('change',function()
+        { onchange(this.value, this.options[this.selectedIndex].text, this.selectedIndex)
 
-                });
-
-
-                var options = select
-                .selectAll('option')
-                    .data(d3.map(dropdownData, function(d, index){ return d;})).enter()
-                    .append('option')
-                    .text(function(d){return d[0];})
-                    .attr("index",function(d, index){return index})
-                    .attr("value",function(d){return d;});
+        });
 
 
-                    function onchange(value, text, index) {
-                        var x1 = d3.scaleTime()
-                            .domain(d3.extent(vaccinedata, function(d) { return new Date(d.Date); }))
-                            .range([ 0, width ]);
+    var options = select
+        .selectAll('option')
+        .data(d3.map(dropdownData, function(d, index){ return d;})).enter()
+        .append('option')
+        .text(function(d){return d[0];})
+        .attr("index",function(d, index){return index})
+        .attr("value",function(d){return d;});
 
-                        var y1 = d3.scaleLinear()
-                            .domain([0, d3.max(vaccinedata, function(d) { return +d.People_Fully_Vaccinated/1000000 || 0; })])
-                            .range([ height, 0 ]);
-                        d3.selectAll("svg").remove();
-                        selectValue = d3.select('select').property('value')
-                        selectIndex = d3.select('select').property('index')
-                        console.log(dropdownData[index]);
+    options.property('selected', function(d){
+        return d[0] === state;
+    })
 
-                        filterResult = getFilteredData(states, vaccinedata, dropdownData[index][0]);
-                        console.log(filterResult);
-                        getLineMap(height, width, margin, filterResult, x, y);
+    function onchange(value, text, index) {
+        var x1 = d3.scaleTime()
+            .domain(d3.extent(vaccinedata, function(d) { return new Date(d.Date); }))
+            .range([ 0, width ]);
+
+        var y1 = d3.scaleLinear()
+            .domain([0, d3.max(vaccinedata, function(d) { return +d.People_Fully_Vaccinated/1000000 || 0; })])
+            .range([ height, 0 ]);
+
+        d3.selectAll("svg").remove();
+        selectValue = d3.select('select').property('value')
+        selectIndex = d3.select('select').property('index')
+        console.log(dropdownData[index]);
+
+        filterResult = getFilteredData(states, vaccinedata, dropdownData[index][0]);
+        console.log(filterResult);
+        getLineMap(height, width, margin, filterResult, x, y);
 
                     };
-    getLineMap(height, width, margin, sorted_countries_byvaccineState, x, y);
-
-    // svg.append('path')
-    //     .data(filterResult[0])
-    //     // .enter().append('rect')
-    //     // .attr('class', 'bar')
-    //     // .enter()
-    //     .attr('fill', 'none')
-    //     .attr("stroke", "red")
-    //     .attr("stroke-width", 1.5)
-    //     .attr("d", d3.line()
-    //         .x(function(d) { return x(d.date) })
-    //         .y(function(d) { return y(d.fully_vaccinated) })
-    //     )
-
-
-
+        getLineMap(height, width, margin, sorted_countries_byvaccineState, x, y);
 
 
 
@@ -104,55 +87,14 @@ function getFilteredData (states, vaccinedata, state) {
 
     return sorted_countries_byvaccineState;
 
-    // var sortedStates = states.features.filter(function(d){
-    //     console.log(d)
-    //     return d.properties.name === state;
-    // });
-
-    console.log(sortedStates);
-
-    // _(states.features)
-    //         .keyBy('properties.name')
-    //         .merge(_.keyBy(vaccinedata, 'Province_State'))
-    //         .values()
-    //         .value();
-
-    // console.log(states);
-
-    // var sorted_countries_byvaccineState = states.features.filter(function(d){
-    //     return d.properties.name === state;
-    // });
-
-    // console.log(sorted_countries_byvaccineState);
-
 }
 
 function getLineMap (height, width, margin, sorted_countries_byvaccineState, x, y) {
     const yAxisTickFormat = number => d3.format("s")(number);
 
-    // svg.append("g")
-    //   .call(d3.axisLeft(y));
-
-    // x.domain(d3.extent(sorted_countries_byvaccineState, function(d) { return d.Date; }));
-    // y.domain([0, d3.max(sorted_countries_byvaccineState, function(d) { return d.People_Fully_Vaccinated; })]);
-
-    // var axis = d3.axisLeft(scale).tickFormat(function(d){
-    //     if(this.parentNode.nextSibling){
-    //    return d/1000000} else { return d/1000000  + " Million"}});
-    //    var yAxis = d3.svg.axis()
-    //    .scale(y)
-    //    .ticks(5)
-    //    .tickFormat(d3.format("s"))
-    //    .orient("left");
-
-    // // var y = d3.scaleLinear()
-    // // .domain([0, 6000000])
-    // // .range([height, ]);
-
-    // var x = d3.scaleBand()
-    // .range([0, width])
-    // .padding(0.1);
-
+    var tooltip = d3.select("body").append("div")
+                      .attr("id", "tooltip")
+                      .style("opacity", 0.8);
     var svg = d3.select('body')
     .append('svg')
     .attr("width", 800)
@@ -160,38 +102,6 @@ function getLineMap (height, width, margin, sorted_countries_byvaccineState, x, 
     .append('g')
     .attr('transform', 'translate('+ margin.left + ',' + margin.top +')');
 
-    // x.domain(sorted_countries_byvaccineState.map(function(d){return d.Date; }));
-    // scale.domain([0, d3.max(sorted_countries_byvaccineState, function(d) {return d.People_Fully_Vaccinated})]);
-    // console.log('hello');
-    // svg.append('g')
-    //     .selectAll("dot")
-    //     .data(sorted_countries_byvaccineState)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", function (d) { return x(d.Date); } )
-    //     .attr("cy", function (d) { console.log(d.People_Fully_Vaccinated || 0); return y(d.People_Fully_Vaccinated || 0 ); } )
-    //     .attr("r", 2)
-    //     .attr("transform", "translate(" + 100 + "," + 100 + ")")
-    //     .style("fill", "#CC0000");
-        // .attr('x', function(d){return x(d.date);})
-        // .attr('width', x.bandwidth())
-        // .attr('y', function(d){return y(d.fully_vaccinated);})
-        // .attr('height', function(d){return height - y(d.fully_vaccinated);})
-        // .attr('fill', 'green');
-
-        // var line = d3.line()
-        // .x(function(d) { return x(d.Date); })
-        // .scale(function(d) { return scale(d.People_Fully_Vaccinated || 0); })
-        // .curve(d3.curveMonotoneX)
-
-        // svg.append("path")
-        // .datum(sorted_countries_byvaccineState)
-        // .attr("class", "line")
-        // .attr("transform", "translate(" + 100 + "," + 100 + ")")
-        // .attr("d", line)
-        // .style("fill", "none")
-        // .style("stroke", "#CC0000")
-        // .style("stroke-width", "2");
     svg.append("path")
         .datum(sorted_countries_byvaccineState)
         .attr("fill", "none")
@@ -200,7 +110,38 @@ function getLineMap (height, width, margin, sorted_countries_byvaccineState, x, 
         .attr("d", d3.line()
           .x(function(d) { console.log(d);return x(new Date(d.Date)) })
           .y(function(d) { console.log(+d.People_Fully_Vaccinated); return y(+d.People_Fully_Vaccinated/1000000 || 0) })
-        )
+        ).on("mouseover", function(event, d) {
+            tooltip.style("display", "flex")
+                   .html(function() {
+              tooltip.attr("country", d.People_Fully_Vaccinated);
+              return `FullyVaccinated: ${Number(d.People_Fully_Vaccinated)}`;})
+                   .style("left", (event.pageX + 10)+"px")
+                   .style("top", (event.pageY - 28) + "px")
+          })
+          .on("mouseout", () => {
+            tooltip.style("display", "none")
+        });
+
+    svg.append("path")
+        .datum(sorted_countries_byvaccineState)
+        .attr("fill", "none")
+        .attr("stroke", "orange")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+          .x(function(d) { console.log(d);return x(new Date(d.Date)) })
+          .y(function(d) { console.log(+d.People_Partially_Vaccinated); return y(+d.People_Partially_Vaccinated/1000000) })
+        ).on("mouseover", function(event, d) {
+            tooltip.style("display", "flex")
+                   .html(function() {
+              tooltip.attr("country", d.People_Partially_Vaccinated);
+              return `PartiallyVaccinated: ${Number(+d.People_Partially_Vaccinated)}`;})
+                   .style("left", (event.pageX + 10)+"px")
+                   .style("top", (event.pageY - 28) + "px")
+          })
+          .on("mouseout", () => {
+            tooltip.style("display", "none")
+        });
+
     svg.append("g")
         .style("display", "none")
         .append("circle")
@@ -209,22 +150,6 @@ function getLineMap (height, width, margin, sorted_countries_byvaccineState, x, 
         .style("stroke", "green")
         .attr("r", 4)
         .style('pointer-events', 'all')
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
-
-    function mousemove() {
-        var x0 = x.invert(d3.mouse(this)[0]),
-            i = bisectDate(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-
-        focus.select("circle.y")
-            .attr("transform",
-                  "translate(" + x(d.date) + "," +
-                                 y(d.close) + ")");
-    }
 
 
 
@@ -236,7 +161,7 @@ function getLineMap (height, width, margin, sorted_countries_byvaccineState, x, 
     .attr("class", "labelforX")
     .attr("x", width)
     .attr("y", height - 6)
-    .text("Months");
+    .text("Months of Years");
 
     svg.append("g")
     .call(d3.axisLeft(y));
@@ -245,15 +170,9 @@ function getLineMap (height, width, margin, sorted_countries_byvaccineState, x, 
     .attr("class", "labelforY")
     .attr("y", 6)
     .attr("dy", ".75em")
+    .style('margin-top', '50px')
+    .style('float', 'right')
     .attr("transform", "rotate(-90)")
-    .text('People Fully Vaccinated in in Millions')
-
-    //     svg.append('g')
-    // .attr('transform', 'translate(0,' + height + ')')
-    // .call(d3.axisBottom(x));
-
-    // svg.append('g')
-    //     .call(axis.tickFormat(yAxisTickFormat));
-    // svg.append("g").attr("transform", "translate(20, 50)").call(axis);
+    .text('Full and Partially Vaccinations (100k-M)');
 
 }
